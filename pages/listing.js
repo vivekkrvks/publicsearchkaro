67b99316a770cbd432a1ada937a5c432f4e62fc5
vivek kumar <vivek.kr.vks@gmail.com>
2022-05-13 00:31:28 +0530

    rating component added

--- pages/listing.js
+++ pages/listing.js
@@ -1,8 +1,9 @@
 import Footer from "../components/HomePage/Footer"
 import Header from "../components/HomePage/Header"
 import { useRouter } from 'next/router'
-import Image from "next/image";
 import CrausalCom from "../components/ListingPage/Crausal";
+import {HeartIcon} from "@heroicons/react/outline"
+import RatingCom from "../components/ListingPage/Rating";
 
 function Search({searchResults}) {
     const router = useRouter();
@@ -12,7 +13,21 @@ function Search({searchResults}) {
         <Header placeholder={`Result for ${searchData}`}/>
 <main className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
 <CrausalCom/>
+<div className="space-y-2 text-xs text-gray-600">
+  <div className="flex mt-2 mb-2 space-x-2">
+  <h1 className="font-bold leading-tight text-2xl  text-black">Tailwind Elements </h1>
+  {/* <HeartIcon className="h-7  cursor-pointer"/> */}
+  </div>
 
+
+            <p>How Search Karo Works </p>
+            <p>
+              <RatingCom star={4} num={4.4}  />
+            </p>
+            <p>Careers</p>
+            <p>Press</p>
+            <p>Investor Relations</p>
+        </div>
 </main>
 
         <Footer />
